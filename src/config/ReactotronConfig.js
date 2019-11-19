0446885d35830d0f1d86da1a18f602e1d155d89e
Jefte <jefte.justino@gmail.com>
2019-11-18 22:31:35 -0300

    Adicionado Profile e Dashboard

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 if (__DEV__) {
   const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
     .configure({ host: '192.168.11.9' })
+    .useReactNative()
     .use(reactotronRedux())
     .use(sagaPlugin())
     .connect();
